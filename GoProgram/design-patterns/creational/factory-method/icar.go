5bb57ac8c1c08c96787d215af0efc857817573b1
dochien0204 <doxuanchienh02042002@gmail.com>
2023-03-06 10:27:18 +0700

    Design Pattern: Factory Method

--- GoProgram/design-patterns/creational/factory-method/icar.go
+++ GoProgram/design-patterns/creational/factory-method/icar.go
@@ -1,8 +1,8 @@
 package factory
 
 type ICar interface {
-	getTypeCar() string
-	getBrand() string
-	setTypeCar(typeCar string)
-	setBrand(brand string)
+	GetTypeCar() string
+	GetBrand() string
+	SetTypeCar(typeCar string)
+	SetBrand(brand string)
 }
